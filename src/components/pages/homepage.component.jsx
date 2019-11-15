c3188fe38bfebcbc8c350cbd4a3f301394fedf99
draddo11 <38097150+draddo11@users.noreply.github.com>
2019-11-15 14:01:51 +0000

    added link for routing

--- src/components/pages/homepage.component.jsx
+++ src/components/pages/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {card} from '../card/card.component';
-
-
+import Drinkspage from '../pages/drinkspage.component';
+import {Route , Link}  from 'react-router-dom';
 
 
 
@@ -10,7 +10,8 @@ class HomePage extends Component {
 render(){
     return(
         <div className='card'>
-        <h1>HomePage component  add <em>{'/drinks'} </em> for drinks facts and <em>{'/cats'}</em> for cat facts</h1>
+        <Link to ='/drinks'><button>To Drinks page</button></Link>
+        <h1>HomePage component  add <em>{'/drinks'} </em> to the url  for drinks facts and <em>{'/cats'}</em>  to the url for cat facts</h1>
         
         
         </div>
